4a00cc1408e63d0049e800556a279fcba092dbfc
lucas_cabrera1 <lucas_cabrera1@hotmail.com>
2022-05-07 16:10:43 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ const App = () => {
   const [countTwo, setCountTwo] = useState(0)
 
   useEffect(() => {
-    console.log('se ejecuta use effect')
-}, [countOne])
+    console.log('se ejecuta use effect al actualizar (tanto incrementar como decrementar) el contador numero 1')
+  }, [countOne])
+
+  useEffect(() => {
+    console.log('se ejecuta use effect al actualizar (tanto incrementar como decrementar) el contador numero 2')
+  }, [countTwo])
 
   const incrementCountOne = () => {
     setCountOne (countOne + 1)
@@ -33,24 +37,30 @@ const App = () => {
       console.log("decrementa el contador 2")
     }
   }
+
+  const reset = () => {
+    setCountOne (0);
+    setCountTwo (0)
+  }
   
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>hola</code> and save to reload.</p>
+        <p>inserte su pronóstico para el siguiente partido</p>
         <div>
           <div className='divcounter'>
-            <img ></img><br/>
+            <span ><img src="./images/river.png" alt='' className='club'></img><br/></span>
             <button onClick={incrementCountOne}>+</button>
-            <p>contador 1 {countOne} veces</p>
+            <p>{countOne}</p>
             <button onClick={decrementCountOne}>-</button>
           </div>
+          <div className='divcounter'> <button onClick={reset}>Reset</button></div>
           <div className='divcounter'>
-            <img src></img><br/>
+            <span><img src = "./images/boca.png" alt='' className='club'></img></span><br/>
             <button onClick={incrementCountTwo}>+</button>
-            <p>contador 2 {countTwo} veces</p>
+            <p>{countTwo}</p>
             <button onClick={decrementCountTwo}>-</button>
           </div>
         </div>
